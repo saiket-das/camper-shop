88a302da4f8277204d16c2f05688e0897b618fe4
saiket-das <saiketdas02@gmail.com>
2024-07-09 06:00:33 +0600

    Add send-response helper & update .gitignore file

--- backend/src/app/modules/product/product.controller.ts
+++ backend/src/app/modules/product/product.controller.ts
@@ -1,11 +1,16 @@
+import httpStatus from "http-status";
 import catchAsync from "../../utils/catchAsync";
 import { ProductServices } from "./product.service";
+import sendResponse from "../../utils/sendResponse";
 
 const createProduct = catchAsync(async (req, res, next) => {
   const result = await ProductServices.createProductService(req.body);
-  if (!result) {
-    console.log("Error");
-  }
+  sendResponse(res, {
+    success: true,
+    statusCode: httpStatus.OK,
+    message: "Product created successfully",
+    data: result,
+  });
 });
 
 export const ProductControllers = {
