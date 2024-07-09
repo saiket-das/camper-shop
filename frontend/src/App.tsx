55c225dc51957b6eae2e1e0a31851eab216717f0
saiket-das <saiketdas02@gmail.com>
2024-07-09 16:37:05 +0600

    Frontend: Implement main-loyout & header component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,10 @@
+import MainLayout from "./components/layout/MainLayout";
+
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    <div>
+      <MainLayout />
+    </div>
   );
 }
 
---
+++ frontend/src/components/layout/MainLayout.tsx
@@ -0,0 +1,11 @@
+import Header from "../ui/Header";
+
+const MainLayout = () => {
+  return (
+    <section className="max-w-7xl mx-auto p-6 lg:px-8">
+      <Header />
+    </section>
+  );
+};
+
+export default MainLayout;
