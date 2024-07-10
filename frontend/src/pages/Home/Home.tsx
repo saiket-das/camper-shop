ca275f4a8d198e2a9fd3af5ed28525c61d8c9fd0
saiket-das <saiketdas02@gmail.com>
2024-07-10 08:04:42 +0600

    Frontend: Add testimonial component to home-page

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,11 +1,13 @@
 import BestProducts from "./components/BestProducts";
 import Hero from "./components/Hero";
+import Testimonial from "./components/Testimonial";
 
 const Home = () => {
   return (
     <div>
       <Hero />
       <BestProducts />
+      <Testimonial />
     </div>
   );
 };
